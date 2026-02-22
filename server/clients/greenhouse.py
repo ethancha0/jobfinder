932355c91d7f29113c1e8ef1c2823faf8184e4b8
ethancha0 <ethanchao2005@gmail.com>
2026-02-22 12:13:46 -0800

    feat: get request for all software jobs, expanded company list to 49

--- server/clients/greenhouse.py
+++ server/clients/greenhouse.py
@@ -1,7 +1,64 @@
 import requests
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+
+from db.database import SessionLocal
+from db.models import Company
 
 router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/alljobs")
+def get_all_greenhouse_jobs(db:Session = Depends(get_db)):
+
+    companies = db.query(Company).all()
+
+    softwareInternJobs = []
+    totalJobsSearched = 0
+    totalCompaniesSearched = 0 
+    totalJobErrors = 0 # invalid slugs 
+
+    for company in companies:
+        url =  f"https://boards-api.greenhouse.io/v1/boards/{company.board_token}/jobs?content=true"
+        response = requests.get(url)
+        totalCompaniesSearched += 1
+
+        if response.status_code != 200:
+            totalJobErrors += 1
+            continue # skip bad jobs for now
+           # return {"error": "Failed to fetch every softare intern jobs"}
+
+        allJobs = response.json()
+        for job in allJobs.get("jobs", []):
+            totalJobsSearched += 1
+            if "software" in job["title"].lower() and "intern" in job["title"].lower():
+                softwareInternJobs.append({
+                    "title": job["title"],
+                    "companyName": job["company_name"],
+                    "location": job["location"],
+                    "published": job["first_published"],
+                    "url": job["absolute_url"]
+                }
+                )
+
+    return {
+        "jobs": softwareInternJobs,
+        "total": len(softwareInternJobs),
+        "totalSearched": totalJobsSearched,
+        "companiesSearched": totalCompaniesSearched,
+    }
+
+
+
 
 
 @router.get("/{company_slug}")
