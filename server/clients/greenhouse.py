15c54c25c36c1b985c924934aeec9a13527c45eb
ethancha0 <ethanchao2005@gmail.com>
2026-02-22 12:40:46 -0800

    fix: db query syntax

--- server/clients/greenhouse.py
+++ server/clients/greenhouse.py
@@ -29,26 +29,33 @@ def get_all_greenhouse_jobs(db:Session = Depends(get_db)):
 
     for company in companies:
         url =  f"https://boards-api.greenhouse.io/v1/boards/{company.board_token}/jobs?content=true"
-        response = requests.get(url)
+        response = requests.get(url, timeout=6)
         totalCompaniesSearched += 1
 
-        if response.status_code != 200:
-            totalJobErrors += 1
-            continue # skip bad jobs for now
-           # return {"error": "Failed to fetch every softare intern jobs"}
+        try:
+            if response.status_code != 200:
+                totalJobErrors += 1
+                continue # skip bad jobs for now
+            # return {"error": "Failed to fetch every softare intern jobs"}
 
-        allJobs = response.json()
-        for job in allJobs.get("jobs", []):
-            totalJobsSearched += 1
-            if "software" in job["title"].lower() and "intern" in job["title"].lower():
-                softwareInternJobs.append({
-                    "title": job["title"],
-                    "companyName": job["company_name"],
-                    "location": job["location"],
-                    "published": job["first_published"],
-                    "url": job["absolute_url"]
-                }
-                )
+            allJobs = response.json()
+            for job in allJobs.get("jobs", []):
+                totalJobsSearched += 1
+                if "software" in job["title"].lower() and "intern" in job["title"].lower():
+                    softwareInternJobs.append({
+                        "title": job["title"],
+                        "companyName": company.name,
+                        "location": job["location"],
+                        "published": job.get("first_published"),
+                        "url": job["absolute_url"]
+                    }
+                    )
+        except requests.exceptions.Timeout:
+            print(f"Timeout for {company}")
+            continue
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed for {company}: {e}")
+            continue
 
     return {
         "jobs": softwareInternJobs,
@@ -65,7 +72,7 @@ def get_all_greenhouse_jobs(db:Session = Depends(get_db)):
 def get_greenhouse_jobs(company_slug: str):
     url = "https://boards-api.greenhouse.io/v1/boards/{company_slug}/jobs?content=true"
     url = url.format(company_slug=company_slug)
-    response = requests.get(url)
+    response = requests.get(url, timeout=6)
     
     if response.status_code != 200:
         return {"error": "Failed to fetch jobs"}
