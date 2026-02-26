9663ad4c3479576d7bd1b43f108cc6942ac4f3e4
ethancha0 <ethanchao2005@gmail.com>
2026-02-26 11:25:39 -0800

    feat: loading message, date formatting

---
+++ frontend/frontend/components/rotating-loading.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+
+const messages =[
+    "Seeding fresh jobs...",
+    "Scanning through 267 companies...",
+    "Crunching the numbers...",
+    "Almost there..."
+
+]
+
+
+const RotatingLoadingMessage = () => {
+    const[index, setIndex] = useState(0)
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setIndex((prev) => (prev + 1) % messages.length)
+        }, 3000) // 3000 ms before updating
+
+        return () => clearInterval(interval)
+
+    },[])
+
+
+
+  return (
+    <div>
+      <p className="text-gray-500 text-sm animate-pulse">{messages[index]}</p>
+    </div>
+  )
+}
+
+export default RotatingLoadingMessage
